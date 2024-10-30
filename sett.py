b9cef654bd3213cc0e2ac22141470bf528c69237
hudayplata <hudaayy14@gmail.com>
2024-10-29 23:55:06 -0500

    first commit

---
+++ sett.py
@@ -0,0 +1,23 @@
+token = 'greengol'
+
+whatsapp_token = 'EAAGEqN6VSOoBO0NZCynYpEloL9YcQ2IU8pgNpSg5ZBAX6Nhtq4SxMc0ncd4CQsxkoFXjYqhYyAex4PZCNFrJH0VMyX51BSFfPE6mgUFzvrKhWlJJs1i9GdquRvP8gMFXbjY7wE4LfQec7oy8CptIaXeaMn4RPvGZCPM9G4ecnduaDQgHSOoZC5TqfEWONUdI7AgZDZD'
+
+whatsapp_url = 'https://graph.facebook.com/v21.0/401657476373844/messages'
+
+stickers = {
+    "poyo_feliz": 984778742532668,
+    "perro_traje": 1009219236749949,
+    "perro_triste": 982264672785815,
+    "pedro_pascal_love": 801721017874258,
+    "pelfet": 3127736384038169,
+    "anotado": 24039533498978939,
+    "gato_festejando": 1736736493414401,
+    "okis": 268811655677102,
+    "cachetada": 275511571531644,
+    "gato_juzgando": 107235069063072,
+    "chicorita": 3431648470417135,
+    "gato_triste": 210492141865964,
+    "gato_cansado": 1021308728970759
+}
+
+document_url = "https://cdn.glitch.global/849c2391-579c-4589-a30c-f1c6c14339d8/Ahorro%20hasta%201300kwh.pdf?v=1730156408245"
