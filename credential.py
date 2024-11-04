97b8198371741d6b1ce5aee1b1c9ad181aa87c98
hudayplata <hudaayy14@gmail.com>
2024-11-03 23:35:46 -0500

    0.9.3

---
+++ credential.py
@@ -0,0 +1,12 @@
+user = "hudaayy14"
+password = "yRn0JcBnMA9AgOIk"
+cluster = "database-greengol-0"
+
+dbname = "whatsapp_conversations"
+<<<<<<< HEAD
+collect = "conversations"
+
+print("")
+=======
+collect = "conversations"
+>>>>>>> 659dbf2f010e6be566adf689eb63a5c9293792a5
---
+++ database.py
@@ -0,0 +1,55 @@
+from pymongo import MongoClient
+import datetime
+import credential
+
+class Conversacion:
+    def __init__(self, number, messageId, username):
+        
+        self.username = username
+        self.number = number
+        self.messageId = messageId
+<<<<<<< HEAD
+        print("")
+=======
+        
+>>>>>>> 659dbf2f010e6be566adf689eb63a5c9293792a5
+        # URI de conexión
+        self.client = MongoClient(f"mongodb+srv://{credential.user}:{credential.password}@{credential.cluster}.amtem.mongodb.net/{credential.dbname}?retryWrites=true&w=majority")
+        self.db = self.client[credential.dbname]
+        self.collection = self.db[credential.collect]
+        
+        
+    def check_User(self):
+        user = self.collection.find_one({"numero_id": self.number})
+        print(user)
+        return user
+
+
+    def new_user(self):
+        user = {
+            "usuario_id": self.username,
+            "numero_id": self.number,
+            #"fecha_inicio": datetime.datetime.now(datetime.timezone.utc),
+            #"fecha_fin": datetime.datetime.now(datetime.timezone.utc) ,
+            "mensajes": []
+        }
+        
+        newuser = self.collection.insert_one(user)
+        return newuser
+    
+    def new_message(self, usertype, text):
+        
+        filtro = {
+            "usuario_id": self.username,
+            "numero_id": self.number
+        }
+        
+        message = {
+            "emisor": usertype,
+            "mensaje": text,
+            "timestamp": datetime.datetime.now(datetime.timezone.utc)
+        }
+        
+        newmessage = self.collection.update_one(filtro, {"$push": {"mensajes": message}})
+
+    
