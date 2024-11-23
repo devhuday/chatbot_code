0e804faff28896ab1039180db9b5e7d3d44dd96f
Glitch (gaudy-magenta-frigate) <none>
2024-11-23 01:14:17 +0000

    🚍👠 Updated with Glitch

--- textbot.py
+++ textbot.py
@@ -1,79 +1,78 @@
 welcome = {
-    "message": "¡Hola! 😊\n\n*¡Bienvenido a Greenglo  S.A.S!* 🌤️\n\nNos alegra que estés aquí. En Greenglo, nos apasiona ayudarte a reducir tu consumo de energía y a cuidar el medio ambiente.",
+    "message": "¡Hola! 😊\n\n*¡Bienvenido a Greenglo S.A.S!* 🌤️\n\nNos alegra que estés aquí. En Greenglo, nos apasiona ayudarte a reducir tu consumo de energía y a cuidar el medio ambiente.",
     "question": "¿Cómo podemos ayudarte hoy?",
-    "option": ["Cotizacion ✍️", "Mantenimiento 🔧"]
-    }
+    "option": ["Cotización ✍️", "Mantenimiento 🔧"]
+}
 
 mantenimientoMens = {
-    "message": "¡Hola! 😊 Gracias por contactarte con *Greengol S.A.S.*\n\nEntendido, deseas agendar una cita para el mantenimiento de una sistema solar con nosotros. Por favor, indícame en un mensaje el motivo de la cita:\n\n*por ejemplo:* Revisión del inversor, mi inversor ha estado comportandose de manera estraña\n\nCon esta información, confirmaremos la disponibilidad y te notificaremos pronto. 🌞🔧."
-    }
+    "message": "¡Hola! 😊 Gracias por contactarte con *Greenglo S.A.S.*\n\nEntendido, deseas agendar una cita para el mantenimiento de un sistema solar con nosotros. Por favor, indícame en un mensaje el motivo de la cita:\n\n*Por ejemplo:* Revisión del inversor, mi inversor ha estado comportándose de manera extraña.\n\nCon esta información, confirmaremos la disponibilidad y te notificaremos pronto. 🌞🔧."
+}
 
 cotizacion = {
-    "message": "Gracias por tu interés en proyectos solares con *Greenglo S.A.S! ☀️ Antes de seguir con la cotizacion necesitamos saber lo siguiente:\n\n *¿Que tipo sistema fotovoltaico tienes en mente implementar?*",
-    "option": ["Sistema On Grid", "Sistema Off Grid", "Sistema Hibrido", "Desconozco estos temas"]
-    }
+    "message": "Gracias por tu interés en proyectos solares con *Greenglo S.A.S*! ☀️ Antes de seguir con la cotización, necesitamos saber lo siguiente:\n\n *¿Qué tipo de sistema fotovoltaico quieres implementar?*",
+    "option": ["Sistema On Grid", "Sistema Off Grid", "Sistema Híbrido", "Desconozco estos temas"]
+}
 
 nameandnumber = {
-    "message": """Antes de seguir su proceso con *Greenglo S.A.S*, por favor necesito que proporcione en un solo mensaje su *nombre, teléfono y correo electrónico.*\n\nEsta informacion sera usada para mejorar su comunicación con el asesor. 
+    "message": """Antes de seguir con tu proceso en *Greenglo S.A.S*, por favor proporciona en un solo mensaje tu *nombre, teléfono y correo electrónico.*\n\nEsta información será usada para mejorar tu comunicación con el asesor. 
 
 *Ejemplo: Luis Gómez, 3700892727, luis@gmail.com.*"""
-    }
+}
 
 agendar = {
-    "message": """Para poder generar su petición de citación, por favor proporcione en un solo mensaje la siguiente informacion: El numero de NIC asociado a su factura de energia y El motivo de la visita para ofrecerle un mayor contexto al asesor.
+    "message": """Para poder generar tu petición de citación, por favor proporciona en un solo mensaje la siguiente información: el número de NIC asociado a tu factura de energía y el motivo de la visita para ofrecerle un mayor contexto al asesor.
     
-*Ejemplo: 7787497, Me interesa las ofertas que me estan dando pero mi vivienda tiene una estructura compleja asi que me gustaría agendar una cita para una evaluación personalizada.*"""
-    }
-
+*Ejemplo: 7787497, Me interesan las ofertas que me están dando, pero mi vivienda tiene una estructura compleja, así que me gustaría agendar una cita para una evaluación personalizada.*"""
+}
 
 cotizacion_grid = {
     "message": "Gracias por tu interés en proyectos solares con *Greenglo S.A.S*! ☀️ Para ayudarte a reducir tu consumo de energía, necesito saber un poco más sobre tu factura.\n\n*¿Podrías decirme qué tipo de factura de energía es?* 😊",
     "option": ["Residencial 🏠", "Comercial 🏢", "Industrial 🏭"]
-    }
+}
 
 cotizacion_offgrid = {
-    "message": "¡Gracias por tu interés en proyectos solares con *Greenglo S.A.S*! ☀️ Para sistemas off grid (aislados), tenemos un catálogo de opciones diseñado para alimentar componentes específicos de tu hogar, local o negocio.\n\nTambien pudes observar una cotizacion de un Sistema de aire acondicionado hibrido para darte una idea de lo que te puedes encontar. \n\n😊 *¡Explora nuestras soluciones y encuentra la ideal para ti!*",
-    "option": ["Sistemas aislados", "Aire hibrido solar"]
-    }
+    "message": "¡Gracias por tu interés en proyectos solares con *Greenglo S.A.S*! ☀️ Para sistemas off grid (aislados), tenemos un catálogo de opciones diseñado para alimentar componentes específicos de tu hogar, local o negocio.\n\nTambién puedes observar una cotización de un sistema de aire acondicionado híbrido para darte una idea de lo que puedes encontrar. \n\n😊 *¡Explora nuestras soluciones y encuentra la ideal para ti!*",
+    "option": ["Sistemas aislados", "Aire híbrido solar"]
+}
 
 cotizacion_hibrido = {
-    "message": "Listo, ahora revisa tu última factura de energía como lo muestra la imagen.\n\nSegun tu consumo actual escoge una de estas tres opciones que tenemos para ti en sistemas hibridos, todas equipadas con respaldo de baterias de hasta 6 horas de duración y con la posibilidad de ampliar el numero de horas de autonomia.",
+    "message": "Listo, ahora revisa tu última factura de energía como lo muestra la imagen.\n\nSegún tu consumo actual, escoge una de estas tres opciones que tenemos para ti en sistemas híbridos, todas equipadas con respaldo de baterías de hasta 6 horas de duración y con la posibilidad de ampliar el número de horas de autonomía.",
     "question": " 📉 *¿Cuál de estas opciones te gustaría conocer mejor?*",
-    "option": ["Ahorro hasta 760kwh", "Ahorro hasta 932kwh", "Ahorro hasta 1350kwh"]
-    }
+    "option": ["Ahorro hasta 760kWh", "Ahorro hasta 932kWh", "Ahorro hasta 1350kWh"]
+}
 
 Residencial_cotizar = {
-    "message": "Listo, ahora revisa tu última factura de energía como lo muestra la imagen.\n\nSegun tu consumo actual escoge una de estas tres opciones que tenemos para ti, todas con beneficios tributarios respaldados por ley: ¡puedes descontar hasta el 50% en renta líquida!",
+    "message": "Listo, ahora revisa tu última factura de energía como lo muestra la imagen.\n\nSegún tu consumo actual, escoge una de estas tres opciones que tenemos para ti, todas con beneficios tributarios respaldados por ley: ¡puedes descontar hasta el 50% en renta líquida!",
     "question": " 📉 *¿Cuál de estas opciones te gustaría conocer mejor?*",
-    "option": ["Ahorro hasta 360kwh", "Ahorro hasta 700kwh", "Ahorro hasta 1000kwh"]
-    } 
+    "option": ["Ahorro hasta 360kWh", "Ahorro hasta 700kWh", "Ahorro hasta 1000kWh"]
+}
 
 Residencial_coti_comercial = {
-    "message": "Listo, ahora revisa tu última factura de energía como lo muestra la imagen.\n\nSegun tu consumo actual escoge una de estas tres opciones que tenemos para ti, todas con beneficios tributarios respaldados por ley: ¡puedes descontar hasta el 50% en renta líquida!",
+    "message": "Listo, ahora revisa tu última factura de energía como lo muestra la imagen.\n\nSegún tu consumo actual, escoge una de estas tres opciones que tenemos para ti, todas con beneficios tributarios respaldados por ley: ¡puedes descontar hasta el 50% en renta líquida!",
     "question": " 📉 *¿Cuál de estas opciones te gustaría conocer mejor?*",
-    "option": ["Ahorro hasta 1300kwh", "Ahorro hasta 1680kwh", "Ahorro hasta 1980kwh"]
-    } 
+    "option": ["Ahorro hasta 1300kWh", "Ahorro hasta 1680kWh", "Ahorro hasta 1980kWh"]
+}
 
 Residencial_coti_mayor = {
-    "message": "Para poder cotizarte necesito la siguiente informacion numero de contacto, copia o foto de la  factura de energía que aparezca el Nic y enviarlo al correo ingenieria@greenglo.com.co o al numero de whatsapp.",
-    } 
+    "message": "Para poder cotizarte, necesito la siguiente información: número de contacto, copia o foto de la factura de energía que muestre el NIC y enviarla al correo ingenieria@greenglo.com.co o al número de WhatsApp."
+}
 
 Residencial_coti_pdf = {
-    "message": "¡Listo! 🎉 Ya puedes descargar tu cotización y revisarla para ver si se ajusta a tu presupuesto. Si tienes alguna otra pregunta o necesitas ajustar algo, aquí estoy para ayudarte. *Nota: La cotización está sujeta a verificación de area y tipo de techo.*"
-    } 
+    "message": "¡Listo! 🎉 Ya puedes descargar tu cotización y revisarla para ver si se ajusta a tu presupuesto. Si tienes alguna otra pregunta o necesitas ajustar algo, aquí estoy para ayudarte. *Nota: La cotización está sujeta a verificación de área y tipo de techo.*"
+}
 
 offgrid_pdf = {
-    "message": "¡Listo! 🎉 Ya puedes descargar El PDF y revisar para ver si se ajusta a tu presupuesto. Si tienes alguna otra pregunta o necesitas ajustar algo, aquí estoy para ayudarte. \n\n*Nota: La cotización está sujeta a verificación de area y tipo de techo.*"
-    } 
- 
-Residencial_coti_negativa = {
-     "message": "Entiendo, si decides no cotizar en este momento.\n\n Si en algún momento cambias de opinión o tienes alguna otra pregunta, no dudes en contactarnos."
-    }
+    "message": "¡Listo! 🎉 Ya puedes descargar el PDF y revisarlo para ver si se ajusta a tu presupuesto. Si tienes alguna otra pregunta o necesitas ajustar algo, aquí estoy para ayudarte. \n\n*Nota: La cotización está sujeta a verificación de área y tipo de techo.*"
+}
 
-Notype = { 
-  "message" : "Lo lamento pero solo puedo leer mensajes de texto 😥. \n\nPero si quieres obtener una cotizacion para tus planes de energia solar no lo pienses dos veces y presiona el boton. 😊🌿"
+Residencial_coti_negativa = {
+    "message": "Entiendo, si decides no cotizar en este momento.\n\nSi en algún momento cambias de opinión o tienes alguna otra pregunta, no dudes en contactarnos."
+}
+
+Notype = {
+    "message": "Lo lamento, pero solo puedo leer mensajes de texto 😥. \n\nSi quieres obtener una cotización para tus planes de energía solar, no lo pienses dos veces y presiona el botón. 😊🌿"
 }
 
 Nomessage = {
-  "message" : "¿Te interesa generar una cotización para tus planes de energía solar? 🤔 ¡Nosotros te podemos ayudar!\n\nSolo necesitas 5 minutos para calcular y cotizar gratis tu proyecto de energía solar a medida. ⏱️"
+    "message": "¿Te interesa generar una cotización para tus planes de energía solar? 🤔 ¡Nosotros te podemos ayudar!\n\nSolo necesitas 5 minutos para calcular y cotizar gratis tu proyecto de energía solar a medida. ⏱️"
 }
