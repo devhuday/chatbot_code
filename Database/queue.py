ae85e692f297c712655fd8ccd676f24a59e790e7
devhuday <hudaayy14@gmail.com>
2024-12-14 14:39:24 -0500

    sda

--- Database/queue.py
+++ Database/queue.py
@@ -6,33 +6,31 @@ from ChatFlow.services import *
 import Database.database as database
 import MessageTools.flow as flow
 
-def load_collect():
-        client = MongoClient(f"mongodb+srv://{user}:{password}@{cluster}.amtem.mongodb.net/{dbname}?retryWrites=true&w=majority")
-        db = client[dbname]
-        collectUsersMessage = db[collectqueue]
-        return collectUsersMessage
+class Queue:
+	def __init__(self):
+		self.client = MongoClient(f"mongodb+srv://{user}:{password}@{cluster}.amtem.mongodb.net/{dbname}?retryWrites=true&w=majority")
+		self.db = self.client[dbname]
+		self.collection = self.db[collect]
+		self.collection_message = self.db[collectqueue]
 
-def load_message(queue,name,number,messageid,text):
-        queue = load_collect()
-        user = {
-            "usuario_id": name, 
-            "numero_id": number,
-            "mensage_id": messageid,
-            "mensage": text,
-            "fecha_inicio": datetime.datetime.now(datetime.timezone.utc),
-        }
-        newuser = queue.insert_one(user)
-        return newuser
+	def load_message(self,name,number,messageid,text):
+		user = {
+			"usuario_id": name, 
+			"numero_id": number,
+			"mensage_id": messageid,
+			"mensage": text,
+			"fecha_inicio": datetime.datetime.now(datetime.timezone.utc),
+		}
+		newuser = self.collection_message.insert_one(user)
+		return newuser
 
-def send_message(queue):
-        queue = load_collect()
-        for doc in queue.find():
-                name=doc.get("usuario_id", None)
-                number=doc.get("numero_id", None)
-                messageId = doc.get("mensage_id", None)
-                text = doc.get("mensage", None)
-                flow.administrar_chatbot(text, number, messageId, name)
+	def send_message(self):
+		for doc in self.collection_message.find():
+			name=doc.get("usuario_id", None)
+			number=doc.get("numero_id", None)
+			messageId = doc.get("mensage_id", None)
+			text = doc.get("mensage", None)
+			flow.administrar_chatbot(text, number, messageId, name)
 
-def verify_queue():
-        collectQueue = load_collect()
-        return collectQueue
+	def verify_queue(self):
+			return self.collection_message
